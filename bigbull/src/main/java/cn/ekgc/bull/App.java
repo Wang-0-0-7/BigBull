bb8be31c6ebe84ea12d907e9fa9e796c957f67c9
Wang <wzh17762201400@163.com>
2022-07-02 11:34:06 +0800

    提交

---
+++ bigbull/src/main/java/cn/ekgc/bull/App.java
@@ -0,0 +1,13 @@
+package cn.ekgc.bull;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
